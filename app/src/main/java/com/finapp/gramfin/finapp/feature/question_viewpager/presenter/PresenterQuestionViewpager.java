40f8d418531b4e4e548c3ae7ed92a1490850fcf0
Helga <olp77@mail.ru>
2020-07-17 16:46:22 +0300

    change API

--- app/src/main/java/com/finapp/gramfin/finapp/feature/question_viewpager/presenter/PresenterQuestionViewpager.java
+++ app/src/main/java/com/finapp/gramfin/finapp/feature/question_viewpager/presenter/PresenterQuestionViewpager.java
@@ -1,13 +1,15 @@
 package com.finapp.gramfin.finapp.feature.question_viewpager.presenter;
 
-import com.finapp.gramfin.finapp.api.question_model.DataRecordRestModel;
-import com.finapp.gramfin.finapp.api.question_model.data_reqord.AnswerRecordRestModel;
+import com.finapp.gramfin.finapp.api.question_model.AllAnswerRestModel;
+
+import com.finapp.gramfin.finapp.api.question_model.PageRestModel;
+
 import com.finapp.gramfin.finapp.feature.question_viewpager.model.ModelQuestion;
 import com.finapp.gramfin.finapp.service.QuestionLoader;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+
 
 import androidx.annotation.Nullable;
 
@@ -15,46 +17,55 @@ public class PresenterQuestionViewpager {
     private static final int QUESTIONS_AMOUNT = 221;
     private static final int QUESTIONS_NEEDED = 10;
 
-    private int chapter_id = 1; // TODO Сейчас доступна только первая тема, для получения списка вопросов будет другое API
+    private int chapter_id;
     private IQuestionViewpager iQuestionViewpager;
     private List<ModelQuestion> questionList = new ArrayList<>();
-    private final Random random = new Random();
 
-    public PresenterQuestionViewpager(IQuestionViewpager iQuestionViewpager) {
+
+    public PresenterQuestionViewpager(IQuestionViewpager iQuestionViewpager, int chapter_id) {
         this.iQuestionViewpager = iQuestionViewpager;
+        this.chapter_id = chapter_id;
         addModelQuestions();
     }
 
     public String calcRightAnswersAmount() {
         int result = 0;
 
-        for (ModelQuestion model:questionList) { if (model.getHaveRightChoice()) result ++; }
+        for (ModelQuestion model : questionList) {
+            if (model.getHaveRightChoice()) result++;
+        }
 
         return String.valueOf(new StringBuilder().append(result).append(" / ").append(QUESTIONS_NEEDED));
     }
 
+
     private void addModelQuestions() {
         QuestionLoader.getInstance()
-                .getDataRecord(chapter_id, random.nextInt(QUESTIONS_AMOUNT), new QuestionLoader.OnRequestListener() {
+                .getDataRecord(chapter_id, new QuestionLoader.OnRequestListener() {
                     @Override
-                    public void onComplete(@Nullable DataRecordRestModel result, String error) {
-                        if (result != null) {
-                            ModelQuestion model = new ModelQuestion(chapter_id, result.id, result.content, result.answers);
-                            model.setChapterName(result.chapters.name);
-                            if (result.topics != null) { model.setTopicName(result.topics.name); }
+                    public void onComplete(@Nullable List<PageRestModel> result, String error) {
 
-                            questionList.add(model);
+
+                        if (result != null) {
+                            for (PageRestModel item : result) {
+                                ModelQuestion model = new ModelQuestion(chapter_id, item.dataQuestion.getDataSubject(), item.dataAnswers);
+                                model.setChapterName(item.dataChapter.name);
+                                if (item.dataTheme != null) {
+                                    model.setTopicName(item.dataTheme.getName());
+                                }
+                                questionList.add(model);
+                            }
+
 
                         } else if (!error.equals(QuestionLoader.QUESTION_NOT_FOUND)) {
-                            ModelQuestion model = new ModelQuestion(chapter_id, -1, error, new ArrayList<AnswerRecordRestModel>());
+                            ModelQuestion model = new ModelQuestion(chapter_id, error, new AllAnswerRestModel());
                             questionList.add(model);
                         }
 
                         if (questionList.size() == QUESTIONS_NEEDED) {
                             QuestionLoader.getInstance().clearCash();
                             iQuestionViewpager.setQuestion(questionList);
-                        }
-                        else addModelQuestions();
+                        } else addModelQuestions();
                     }
                 });
     }
