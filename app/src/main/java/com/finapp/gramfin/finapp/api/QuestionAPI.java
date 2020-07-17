1b009f4bfc35503f9903e973010cea9d2672b04c
Helga <olp77@mail.ru>
2020-07-17 16:33:33 +0300

    change API

--- app/src/main/java/com/finapp/gramfin/finapp/api/QuestionAPI.java
+++ app/src/main/java/com/finapp/gramfin/finapp/api/QuestionAPI.java
@@ -2,10 +2,14 @@ package com.finapp.gramfin.finapp.api;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+
 import com.finapp.gramfin.finapp.api.question_model.PageRestModel;
 
+import java.util.List;
+
 public interface QuestionAPI {
-    @GET("api/question")
-    Call<PageRestModel> loadQuestions(@Query("page") int page);
+    @GET("/question/{chapter}")
+    Call<List<PageRestModel>> loadQuestions(@Path("chapter") int chapterForTraining, @Header("Authorization") String token);
 }
