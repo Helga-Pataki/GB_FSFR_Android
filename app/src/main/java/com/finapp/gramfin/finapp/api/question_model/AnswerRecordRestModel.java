423bdbabf76bda20d94eda4263cbe8b4624a383e
Helga <olp77@mail.ru>
2020-08-18 22:17:13 +0300

    refactor

--- app/src/main/java/com/finapp/gramfin/finapp/api/question_model/AnswerRecordRestModel.java
+++ app/src/main/java/com/finapp/gramfin/finapp/api/question_model/AnswerRecordRestModel.java
@@ -1,4 +1,4 @@
-package com.finapp.gramfin.finapp.api.question_model.data_reqord;
+package com.finapp.gramfin.finapp.api.question_model;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
