23e86d17a82031b93d3705a7ad2244e9dae086f9
Helga <olp77@mail.ru>
2020-08-18 23:05:13 +0300

    загрузка глав с сервера

--- app/src/main/java/com/finapp/gramfin/finapp/feature/second_screen/model/ModelChapter.java
+++ app/src/main/java/com/finapp/gramfin/finapp/feature/second_screen/model/ModelChapter.java
@@ -3,23 +3,26 @@ package com.finapp.gramfin.finapp.feature.second_screen.model;
 public class ModelChapter {
 
     private String chapter;
-    private int numberOfQuetions;
+    private int numberChapter;
+    private int totalOfQuestions;
     private int itemIKnow;
     private int itemIdontKnow;
 
-    public ModelChapter(String chapter, int numberOfQuetions, int itemIKnow, int itemIdontKnow) {
+    public ModelChapter(String chapter, int numberChapter) {//public ModelChapter(String chapter, int numberOfQuetions, int itemIKnow, int itemIdontKnow)
         this.chapter = chapter;
-        this.numberOfQuetions = numberOfQuetions;
-        this.itemIKnow = itemIKnow;
-        this.itemIdontKnow = itemIdontKnow;
+        this.numberChapter = numberChapter;
+       // this.totalOfQuestions = totalOfQuestions;
+
+        // this.itemIKnow = itemIKnow;
+        // this.itemIdontKnow = itemIdontKnow;
     }
 
     public String getChapter() {
         return chapter;
     }
 
-    public int getNumberOfQuetions() {
-        return numberOfQuetions;
+    public int getTotalOfQuestions() {
+        return totalOfQuestions;
     }
 
     public int getItemIKnow() {
@@ -28,5 +31,25 @@ public class ModelChapter {
 
     public int getItemIdontKnow() {
         return itemIdontKnow;
+    }
+    public void setChapter(String chapter) {
+        this.chapter = chapter;
+    }
+
+    public void setNumberChapter(int numberChapter) {
+        this.numberChapter = numberChapter;
+    }
+
+    public void setTotalOfQuestions(int totalOfQuestions) {
+        this.totalOfQuestions = totalOfQuestions;
+    }
+
+    public void setItemIKnow(int itemIKnow) {
+        this.itemIKnow = itemIKnow;
+
+
+    }
+    public void setItemIdontKnow(int itemIdontKnow) {
+        this.itemIdontKnow = itemIdontKnow;
     }
 }
